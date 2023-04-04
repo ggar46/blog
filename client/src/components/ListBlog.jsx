adf1862a2569f8f688d66dfe4bb0799b82e09cda
Gisselle Garcia <gissellegar46@gmail.com>
2023-04-03 19:00:13 -0700

    Remove delete functionality temporarily

--- client/src/components/ListBlog.jsx
+++ client/src/components/ListBlog.jsx
@@ -8,8 +8,6 @@ const ListStudents = () => {
     // this is my original state with an array of students 
     const [students, setStudents] = useState([]);
 
-    //this is the state needed for the UpdateRequest
-    const [editingStudent, setEditingStudent] = useState(null)
 
     const loadStudents = () => {
         // A function to fetch the list of students that will be load anytime that list change
@@ -24,25 +22,25 @@ const ListStudents = () => {
         loadStudents();
     }, [students]);
 
+
     const onSaveStudent = (newStudent) => {
         //console.log(newStudent, "From the parent - List of Students");
         setStudents((students) => [...students, newStudent]);
     }
 
 
-
     //A function to handle the Delete funtionality
-    const onDelete = (student) => {
-        //console.log(student, "delete method")
-        return fetch(`http://localhost:8080/api/students/${student.id}`, {
-            method: "DELETE"
-        }).then((response) => {
-            //console.log(response);
-            if (response.ok) {
-                loadStudents();
-            }
-        })
-    }
+    // const onDelete = (student) => {
+    //     //console.log(student, "delete method")
+    //     return fetch(`http://localhost:8080/api/students/${student.id}`, {
+    //         method: "DELETE"
+    //     }).then((response) => {
+    //         //console.log(response);
+    //         if (response.ok) {
+    //             loadStudents();
+    //         }
+    //     })
+    // }
 
    
 
